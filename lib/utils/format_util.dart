92c01319576045f176d1fefcf77f79761c63096a
WuHaiheng <wuhh@csco.com.cn>
2023-02-17 15:16:36 +0800

    首页 -- 首页列表卡片组件完善

---
+++ lib/utils/format_util.dart
@@ -0,0 +1,24 @@
+/// 数字转万
+String countMillionFormat(int count) {
+  String result = "";
+  if (count > 9999) {
+    // 保留两位小数
+    result = "${(count / 10000).toStringAsFixed(2)}万";
+  } else {
+    result = count.toString();
+  }
+  return result;
+}
+
+/// 将描述转成【分：秒】格式
+String timeDurationFormat(int seconds) {
+  String result = "";
+  int m = (seconds / 60).truncate();
+  int s = seconds - m * 60;
+  if (s < 10) {
+    result = '$m:0$s';
+  } else {
+    result = '$m:$s';
+  }
+  return result;
+}
--- lib/widget/video_card.dart
+++ lib/widget/video_card.dart
@@ -1,23 +1,150 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bili_app/navigator/hi_navigator.dart';
 import 'package:flutter_bili_app/utils/LogUtil.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 import '../http/model/home_mo.dart';
+import '../utils/format_util.dart';
 
 class VideoCard extends StatelessWidget {
   final String tag = 'VideoCard';
-  final VideoMo? videoMo;
+  final VideoMo videoMo;
 
-  const VideoCard({Key? key, this.videoMo}) : super(key: key);
+  const VideoCard({Key? key, required this.videoMo}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: InkWell(
-        onTap: () {
-          LogUtil.L(tag, "url:${videoMo?.url}");
-        },
-        child: Image.network(videoMo?.cover! ?? '',fit:BoxFit.fill,),
+    return InkWell(
+      onTap: () {
+        LogUtil.L(tag, "url:${videoMo.url}");
+        HiNavigator.getInstance().onJumpTo(RouteStatus.detail, args: {"videoMo": videoMo});
+      },
+      child: SizedBox(
+        height: 200,
+        child: Card(
+          // 取消卡片边const 距
+          margin: const EdgeInsets.only(left: 4, right: 4, bottom: 8),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _itemImage(MediaQuery.of(context).size),
+                _infoText(),
+              ],
+            ),
+          ),
+        ),
       ),
+    );
+  }
+
+  _itemImage(Size size) {
+    return Stack(
+      children: [
+        FadeInImage.memoryNetwork(
+          height: 120,
+          width: (size.width / 2 - 20),
+          placeholder: kTransparentImage,
+          image: videoMo.cover ?? "",
+          fit: BoxFit.cover,
+        ),
+        Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              padding: const EdgeInsets.only(left: 6, right: 6, bottom: 2, top: 4),
+              decoration: const BoxDecoration(
+                  // 渐变
+                  gradient: LinearGradient(begin: Alignment.bottomCenter, end: Alignment.topCenter, colors: [Colors.black54, Colors.transparent])),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                // 垂直居中
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  _iconText(iconData: Icons.ondemand_video, count: videoMo.view!),
+                  _iconText(iconData: Icons.favorite_border, count: videoMo.favorite!),
+                  _iconText(count: videoMo.duration!),
+                ],
+              ),
+            ))
+      ],
+    );
+  }
+
+  _iconText({IconData? iconData, required int count}) {
+    String views = "";
+    if (iconData != null) {
+      views = countMillionFormat(count);
+    } else {
+      views = timeDurationFormat(videoMo.duration!);
+    }
+    return Row(
+      // 垂直居中
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        if (iconData != null) Icon(iconData, color: Colors.white, size: 12),
+        Padding(
+          padding: const EdgeInsets.only(left: 4),
+          child: Text(
+            views,
+            style: const TextStyle(fontSize: 10, color: Colors.white),
+          ),
+        )
+      ],
+    );
+  }
+
+  _infoText() {
+    return Expanded(
+        child: Container(
+      padding: const EdgeInsets.only(left: 6, right: 6, bottom: 4, top: 4),
+      child: Column(
+        // 左边对齐
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        // 文字最多2行，多的用..显式
+        children: [
+          Text(
+            videoMo.title!,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: const TextStyle(fontSize: 12, color: Colors.black),
+          ),
+          _owner()
+        ],
+      ),
+    ));
+  }
+
+  _owner() {
+    var owner = videoMo.owner;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.network(
+                owner?.face ?? "",
+                width: 24,
+                height: 24,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8),
+              child: Text(
+                owner?.name ?? "",
+                style: const TextStyle(fontSize: 12, color: Colors.black),
+              ),
+            )
+          ],
+        ),
+        const Icon(Icons.more_vert_sharp, size: 15, color: Colors.grey)
+      ],
     );
   }
 }
